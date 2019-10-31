8990f1cb596c7e4ccfb6a7d11cd70f305a506caa
Huy Truong <huy.truong@mevry.com>
2019-10-31 16:55:21 +0700

    socket.io events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-io()
+const socket = io()
+
+socket.on('countUpdated', (count)=>{
+    console.log('The count have been Updated!', count)
+})
+
+document.querySelector('#increment').addEventListener('click',()=>{
+    console.log('Clicked')
+
+    socket.emit('increment')
+})
