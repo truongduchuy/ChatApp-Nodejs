61e8513741740f3677b12c02bf8d53593b0d01da
Huy Truong <huy.truong@mevry.com>
2019-10-31 23:30:31 +0700

    events acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,7 +10,12 @@ document.querySelector('#message-form').addEventListener('submit', e => {
     // const message = document.querySelector('input').value
     const message = e.target.elements.message.value
     
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error) => {
+        if(error)
+        return console.log(error)
+
+        console.log('Message delivered!')
+    })
 
 })
 
@@ -24,6 +29,7 @@ document.querySelector('#send-location').addEventListener('click', () =>{
         {
             latitude: position.coords.latitude, 
             longitude: position.coords.longitude
-        })
+        }, () => console.log('Location shared!')
+        )
     })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -19,16 +20,23 @@ io.on('connection', (socket) =>{
     socket.emit('greeting', 'Welcome!');
     socket.broadcast.emit('greeting', 'A new user has joined!')
 
-    socket.on('sendMessage', message =>{
+    socket.on('sendMessage', (message, callback) =>{
+        const filter = new Filter()
+        
+        if(filter.isProfane(message))
+            return callback('Profanity is not allowed!')
+
         io.emit('greeting', message)
+        callback()
     })
 
     socket.on('disconnect', ()=>{
         io.emit('greeting', 'A user has left')
     })
 
-    socket.on('sendLocation', coords => {
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('greeting', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
 })
 
