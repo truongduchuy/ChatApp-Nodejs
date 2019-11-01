1e1b04f7792da872ab749ab27ac372c9550d7ea4
Huy Truong <huy.truong@mevry.com>
2019-11-01 10:56:41 +0700

    rendering location messages

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) =>{
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('greeting', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
         callback()
     })
 })
